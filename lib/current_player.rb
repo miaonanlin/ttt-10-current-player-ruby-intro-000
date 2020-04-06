7e41da1a57566b6a5b9f6f3dcc61b48a682d75cd
Miao Nan Lin <miaonanlin@gmail.com>
2020-04-06 20:35:00 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,9 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0 
+  if turn_count(board) % 2 == 0
     return "X"
-  else 
+  else
     return "O"
-  end 
+  end
 end
